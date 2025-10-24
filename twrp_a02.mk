c55fd6fa02e46581b1fc756c94757a09c79c81a6
a02-trial <cipin23@gmail.com>
2025-10-25 03:10:32 +0700

    Update and rename omni_a02.mk to twrp_a02.mk

--- twrp_a02.mk
+++ twrp_a02.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a02 device
 $(call inherit-product, device/samsung/a02/device.mk)
 
 PRODUCT_DEVICE := a02
-PRODUCT_NAME := omni_a02
+PRODUCT_NAME := twrp_a02
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A022F
 PRODUCT_MANUFACTURER := samsung
@@ -23,6 +23,6 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a02xx-user 11 RP1A.200720.012 A022FXXU3BWB5 release-keys"
+    PRIVATE_BUILD_DESC="a02xx-user 11 RP1A.200720.012 A022FXXS3BVB1 release-keys"
 
-BUILD_FINGERPRINT := samsung/a02xx/a02:11/RP1A.200720.012/A022FXXU3BWB5:user/release-keys
+BUILD_FINGERPRINT := samsung/a02xx/a02:11/RP1A.200720.012/A022FXXS3BVB1:user/release-keys
