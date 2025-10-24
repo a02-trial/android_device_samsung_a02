93f82dcbc2e959054e02c41a5872607fb8c17be8
a02-trial <cipin23@gmail.com>
2025-10-25 03:20:18 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,16 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := false
-TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+RECOVERY_SDCARD_ON_DATA := false
+TW_HAS_DOWNLOAD_MODE := true
+TW_DISABLE_TTF := true
+TW_DEVICE_VERSION := a02_trial
+
+# TWRP trial Configuration
+TWHAVE_SELINUX := true
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/samsung_mobile_device/samsung_mobile_device:dc_vibrator/leds/vibrator/brightness"
+TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/samsung_mobile_device/samsung_mobile_device:battery/power_supply/battery"
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := false
