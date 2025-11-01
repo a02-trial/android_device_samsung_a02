569ea1ba4b6931ab5cd60f4c5c8cf1745cd80ff6
a02-trial <cipin23@gmail.com>
2025-11-02 05:34:10 +0700

    Update and rename omni_a02.mk to pb_a02.mk

--- pb_a02.mk
+++ pb_a02.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common pb stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from a02 device
 $(call inherit-product, device/samsung/a02/device.mk)
 
 PRODUCT_DEVICE := a02
-PRODUCT_NAME := omni_a02
+PRODUCT_NAME := pb_a02
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A022F
 PRODUCT_MANUFACTURER := samsung
