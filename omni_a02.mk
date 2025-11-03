4abf486cc88a679a4f64570f07eddabd2639e9ae
a02-trial <cipin23@gmail.com>
2025-11-04 01:58:34 +0700

    Update omni_a02.mk

--- omni_a02.mk
+++ omni_a02.mk
@@ -7,7 +7,7 @@
 
 # Inherit some common Omni stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a02 device
 $(call inherit-product, device/samsung/a02/device.mk)
