f65756e3be73907d81e8ee091fa505cc3ce18d23
a02-trial <cipin23@gmail.com>
2025-11-04 10:29:00 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,6 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
-TARGET_2ND_CPU_VARIANT := cortex-a53
 
 BOARD_VENDOR := samsung
 TARGET_SOC := k39tv1_bsp_titan_hamster
