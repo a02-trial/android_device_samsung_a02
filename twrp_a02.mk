ba5a40aa9f1be406640904a0ef5358fb45bff994
a02-trial <cipin23@gmail.com>
2025-11-11 00:32:52 +0700

    Update twrp_a02.mk

--- twrp_a02.mk
+++ twrp_a02.mk
@@ -9,13 +9,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a02 device
 $(call inherit-product, device/samsung/a02/device.mk)
 
 PRODUCT_DEVICE := a02
-PRODUCT_NAME := omni_a02
+PRODUCT_NAME := twrp_a02
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A022F
 PRODUCT_MANUFACTURER := samsung
